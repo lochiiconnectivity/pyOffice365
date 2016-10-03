c08e22d95349a139165e88aacc0072d6cae1d603
lochiiconnectivity <lcreg-github@convergence.cx>
2016-10-03 10:57:42 +0100

    first with devel_crest

--- pyOffice365.py
+++ pyOffice365.py
@@ -7,81 +7,113 @@ import urllib
 import urllib2
 import uuid
 
+
 class pyOffice365():
 
     __re_skiptoken = re.compile('.*\$skiptoken=([^&]*).*')
+    __graph_api_endpoint = 'https://graph.windows.net'
+    __graph_api_version = '1.5'
+    __crest_api_endpoint = 'https://api.cp.microsoft.com'
+    __crest_api_version = '2015-03-31'
+    __oauth2_api_endpoint = 'https://login.windows.net'
 
-    def __init__(self, domain, debug=False, apiversion='1.5'):
-        self.debug = debug
-        self.domain = domain
+    def __init__(self, domain, debug_requests=False, debug_responses=False,
+                 graph_api_endpoint=__graph_api_endpoint,
+                 graph_api_version=__graph_api_version,
+                 crest_api_endpoint=__crest_api_endpoint,
+                 crest_api_version=__crest_api_version,
+                 oauth_api_endpoint=__oauth2_api_endpoint):
+
+        self.__debug_requests = debug_requests
+        self.__debug_responses = debug_responses
+        self.__graph_api_endpoint = graph_api_endpoint
+        self.__graph_api_version = graph_api_version
+        self.__crest_api_endpoint = crest_api_endpoint
+        self.__crest_api_version = crest_api_version
+        self.__oauth2_api_endpoint = oauth_api_endpoint
+        self.__domain = domain
         self.__access_token = None
         self.__crest_sa_token = None
+        self.__customer_token = None
         self.__crest_reseller_id = None
+        self.__crest_tenant_id = None
         self.__ms_tracking_id = uuid.uuid4()
-        self.apiversion = apiversion
-        if debug:
+
+        if debug_requests:
             urllib2.install_opener(urllib2.build_opener(urllib2.HTTPSHandler(debuglevel=1)))
 
-    def login(self, user, passwd, resource='https://graph.windows.net'):
+    def graph_login(self, user, passwd, resource=None):
+        if resource is None:
+            resource = self.__graph_api_endpoint
         postData = {
             "grant_type": "client_credentials",
             "resource": resource,
-            "client_id": "%s@%s" % (user, self.domain),
+            "client_id": "%s@%s" % (user, self.__domain),
             "client_secret": passwd,
         }
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
         }
-        req = urllib2.Request("https://login.windows.net/%s/oauth2/token?api-version=1.0" % (self.domain), urllib.urlencode(postData), headers)
+        req = urllib2.Request("%s/%s/oauth2/token?api-version=1.0" % (self.__oauth2_api_endpoint, self.__domain), urllib.urlencode(postData), headers)
         u = urllib2.urlopen(req)
         data = u.readlines()
+        if self.__debug_responses is True:
+            print data
         jdata = json.loads('\n'.join(data))
         if jdata.has_key("access_token"):
             self.__access_token =  jdata["access_token"]
 
     def crest_login(self):
-        tenant_id = self.get_tenant()['value'][0]['objectId']
+        self.__crest_tenant_id = self.get_tenant()['value'][0]['objectId']
 
-        req = urllib2.Request("https://api.cp.microsoft.com/my-org/tokens", 'grant_type=client_credentials', \
+        req = urllib2.Request("%s/my-org/tokens" % self.__crest_api_endpoint, 'grant_type=client_credentials', \
             headers=self.__auth_header__(accept='application/json', content_type='application/x-www-form-urlencoded'))
         u = urllib2.urlopen(req)
         data = u.readlines()
+        if self.__debug_responses is True:
+            print data
         jdata = json.loads('\n'.join(data))
         if jdata.has_key("access_token"):
             self.__crest_sa_token =  jdata["access_token"]
 
-        req = urllib2.Request("https://api.cp.microsoft.com/customers/get-by-identity?provider=AAD&type=tenant&tid=%s" % tenant_id, \
+        req = urllib2.Request("%s/customers/get-by-identity?provider=AAD&type=tenant&tid=%s" % (self.__crest_api_endpoint, self.__crest_tenant_id) ,
             headers=self.__crest_auth_header__(accept='application/json'))
         u = urllib2.urlopen(req)
         data = u.readlines()
+        if self.__debug_responses is True:
+            print data
         jdata = json.loads('\n'.join(data))
         if jdata.has_key("id"):
             self.__crest_reseller_id =  jdata["id"]
 
 
-    def __auth_header__(self, accept='application/json;odata=nometadata', content_type='application/json;odata=nometadata'):
+    def __auth_header__(self, accept='application/json;odata=nometadata', content_type='application/json;odata=nometadata', authorization=None):
+        if authorization is None:
+            authorization=self.__access_token
         return {
-            "Authorization": "Bearer %s" % (self.__access_token),
+            "Authorization": "Bearer %s" % (authorization),
             "Accept": accept, 
             "Content-Type": content_type,
             "x-ms-correlation-id": uuid.uuid4(),
             "x-ms-tracking-id": self.__ms_tracking_id,
         }
 
-    def __crest_auth_header__(self, accept='application/json;odata=nometadata', content_type='application/json;odata=nometadata'):
+    def __crest_auth_header__(self, accept='application/json;odata=nometadata', content_type='application/json;odata=nometadata', authorization=None):
+        if authorization is None:
+            authorization=self.__crest_sa_token
         return {
-            "Authorization": "Bearer %s" % (self.__crest_sa_token),
+            "Authorization": "Bearer %s" % (authorization),
             "Accept": accept, 
-            "api-version" : "2015-03-31",
+            "api-version" : self.__crest_api_version,
             "Content-Type": content_type,
             "x-ms-correlation-id": uuid.uuid4(),
             "x-ms-tracking-id": self.__ms_tracking_id,
         }
 
     def __doreq__(self, command, postdata=None, querydata={}, method=None):
-        querydata['api-version'] = self.apiversion
+        querydata['api-version'] = self.__graph_api_version
         
-        req = urllib2.Request("https://%s/%s/%s?%s" % ('graph.windows.net', self.domain, command, urllib.urlencode(querydata)), data=postdata, headers=self.__auth_header__())
+        req = urllib2.Request("%s/%s/%s?%s" % (self.__graph_api_endpoint, self.__domain, command, urllib.urlencode(querydata)), data=postdata, headers=self.__auth_header__())
 
         if method is not None:
             req.get_method = lambda: method
@@ -90,6 +122,8 @@ class pyOffice365():
             u = urllib2.urlopen(req)
         except urllib2.HTTPError, e:
             data = e.readlines()
+            if self.__debug_responses is True:
+                print data
             try:
                 jdata = json.loads('\n'.join(data))
             except:
@@ -97,17 +131,24 @@ class pyOffice365():
             return jdata
 
         data = u.readlines()
+        if self.__debug_responses is True:
+            print data
+
         try:
             jdata = json.loads('\n'.join(data))
         except:
             jdata = data
         return jdata
 
-    def __crest_doreq__(self, command, postdata=None, querydata={}, method=None):
+    def __crest_doreq__(self, command, postdata=None, querydata={}, method=None, resellerPath=True, token=None):
         if self.__crest_reseller_id is None:
             self.crest_login()
 
-        req = urllib2.Request("https://%s/%s/%s?%s" % ('api.cp.microsoft.com', self.__crest_reseller_id, command, urllib.urlencode(querydata)), data=postdata, headers=self.__crest_auth_header__())
+        req = None
+        if resellerPath is True:
+            req = urllib2.Request("%s/%s/%s?%s" % (self.__crest_api_endpoint, self.__crest_reseller_id, command, urllib.urlencode(querydata)), data=postdata, headers=self.__crest_auth_header__(authorization=token))
+        else:
+            req = urllib2.Request("%s/%s?%s" % (self.__crest_api_endpoint, command, urllib.urlencode(querydata)), data=postdata, headers=self.__crest_auth_header__(authorization=token))
 
         if method is not None:
             req.get_method = lambda: method
@@ -116,6 +157,8 @@ class pyOffice365():
             u = urllib2.urlopen(req)
         except urllib2.HTTPError, e:
             data = e.readlines()
+            if self.__debug_responses is True:
+                print data
             try:
                 jdata = json.loads('\n'.join(data))
             except:
@@ -123,11 +166,32 @@ class pyOffice365():
             return jdata
 
         data = u.readlines()
+        if self.__debug_responses is True:
+            print data
         try:
             jdata = json.loads('\n'.join(data))
         except:
             jdata = data
         return jdata
+
+    def __crest_get_customer__(self, tid=None):
+        if self.__crest_tenant_id is None:
+            self.crest_login()
+
+        customer = self.__crest_doreq__("customers/get-by-identity", querydata={"provider": "AAD", "type": "external_group", "tid": tid, "etid": self.__crest_tenant_id}, resellerPath=False)
+
+        if customer:
+            req = urllib2.Request("%s/%s/tokens" % (self.__crest_api_endpoint, customer["id"]), 'grant_type=client_credentials', \
+                headers=self.__auth_header__(accept='application/json', content_type='application/x-www-form-urlencoded'))
+            u = urllib2.urlopen(req)
+            data = u.readlines()
+            if self.__debug_responses is True:
+                print data
+            jdata = json.loads('\n'.join(data))
+            if jdata.has_key("access_token"):
+                self.__customer_token = jdata["access_token"]
+            
+        return customer
 
     def get_tenant(self):
         return self.__doreq__("tenantDetails")
@@ -135,6 +199,30 @@ class pyOffice365():
     def get_contracts(self):
         return self.__doreq__("contracts")
 
+    def get_orders(self, tid=None):
+
+        querydata = {}
+        rdata = []
+
+        customer = self.__crest_get_customer__(tid=tid)
+    
+        if customer:
+            orders_path = "orders"
+            querydata = {"recipient_customer_id": customer["id"]}
+            while True:
+                data = self.__crest_doreq__(orders_path, querydata=querydata, token=None)
+                if type(data) != types.DictType:
+                    return None
+                if 'items' in data:
+                    rdata += [data]
+                if 'odata.nextLink' in data:
+                    skiptoken = self.__re_skiptoken.search(data["odata.nextLink"]).group(1)
+                    querydata["$skiptoken"] = skiptoken
+                else:
+                    break
+
+        return rdata
+        
     def get_users(self, user=None):
         querydata = {}
         rdata = []
@@ -143,14 +231,13 @@ class pyOffice365():
             if '@' in user:
                 users_path = "users/%s" % (user)
             else:
-                users_path = "users/%s@%s" % (user, self.domain)
+                users_path = "users/%s@%s" % (user, self.__domain)
         else:
             users_path = "users"
 
         while True:
             data = self.__doreq__(users_path, querydata=querydata)
             if type(data) != types.DictType:
-                print data
                 return None
             if 'userPrincipalName' in data:
                 rdata += [data]
@@ -177,7 +264,7 @@ class pyOffice365():
         if '@' in username:
             return self.__doreq__("users/%s" % (username), postdata=json.dumps(userdata), method='PATCH')
         else:
-            return self.__doreq__("users/%s@%s" % (username, self.domain), postdata=json.dumps(userdata), method='PATCH')
+            return self.__doreq__("users/%s@%s" % (username, self.__domain), postdata=json.dumps(userdata), method='PATCH')
 
     def assign_license(self, username, sku=None, disabledplans=None, remove=None):
         postData = {
@@ -193,4 +280,4 @@ class pyOffice365():
         if '@' in username:
             return self.__doreq__("users/%s/assignLicense" % (username), postdata=json.dumps(postData))
         else:
-            return self.__doreq__("users/%s@%s/assignLicense" % (username, self.domain), postdata=json.dumps(postData))
+            return self.__doreq__("users/%s@%s/assignLicense" % (username, self.__domain), postdata=json.dumps(postData))
